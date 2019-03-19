9d3cd2b52823bb4ca1530f1db95a110097d6759f
Martin <martinius96@gmail.com>
2019-03-19 17:12:19 +0100

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,2 @@
+gcc hashchain.c -o hashchain.exe -lcrypto -I C:\BATCHES\include
+pause
---
+++ sha256.bat
@@ -0,0 +1,4 @@
+hashchain.exe create sha256 10 "a"
+pause
+hashchain.exe verify sha256 t0WBOjZgDX9GnSIX8otKHsfctkEVIW0Inznsdn8Ez0w= PFk1z0lYwYskmc/yLcri2JQpnwIlFtjQxlpSm9T9eMU=
+pause
